50bcb826d2252046dd815648b77b7977ceca59d9
gabrielsidauruk <gpratama796@gmail.com>
2023-03-06 23:07:42 +0700

    Update database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('sistem-pakar-skripsi','postgres','gabrielsidauruk',{
-    host:'localhost',
-    dialect:'postgres'
+const db = new Sequelize('bnj1oun0ufiikmweczhj', 'urtphz16jxw2cvaofmxa', 'IlKZ958UPu3ePzMCTaeNTuhZgddw2K', {
+    host: 'bnj1oun0ufiikmweczhj-postgresql.services.clever-cloud.com',
+    dialect: 'postgres'
 });
 
 export default db;
