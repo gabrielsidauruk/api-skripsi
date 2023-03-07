5d105543ede48a7fc1d42941b54b49af1f3bfe6d
gabrielsidauruk <gpratama796@gmail.com>
2023-03-07 16:39:23 +0700

    hadeh error lagi

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('bnj1oun0ufiikmweczhj', 'urtphz16jxw2cvaofmxa', 'IlKZ958UPu3ePzMCTaeNTuhZgddw2K', {
     host: 'bnj1oun0ufiikmweczhj-postgresql.services.clever-cloud.com',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
 });
 
 export default db;
