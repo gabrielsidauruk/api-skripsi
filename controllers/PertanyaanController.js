6a5eef7278ac0d0853bc795651559bc9fa71854d
gabrielsidauruk <gpratama796@gmail.com>
2023-03-07 21:26:56 +0700

    pertanyaan controller async function

--- controllers/PertanyaanController.js
+++ controllers/PertanyaanController.js
@@ -1,6 +1,6 @@
 import Pertanyaan from "../models/PertanyaanModel.js";
 
-export const getPertanyaan = async (req, res) => {
+export const getPertanyaan = async function (req, res) {
     try {
         const response = await Pertanyaan.findAll();
         res.status(200).json(response);
