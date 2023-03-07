caa546a30a441547624f7f496177e4df672492e3
gabrielsidauruk <gpratama796@gmail.com>
2023-03-07 14:54:32 +0700

    export default app

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 import PertanyaanRoute from "./routes/PertanyaanRoute.js"
-
-
 const app = express();
 const port = 8000;
+
+
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
@@ -13,3 +13,8 @@ app.use(PertanyaanRoute);
 app.listen(port, () => {
     console.log('App dijalankan di port', port)
 });
+app.get("/", (req, res) => {
+    res.send("hello from express")
+})
+
+export default app
