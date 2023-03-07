f105261eab9430b40a8a2e061ed2d843eb0de639
gabrielsidauruk <gpratama796@gmail.com>
2023-03-07 18:58:30 +0700

    edit dotenv

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
 import pg from "pg";
 
-const db = new Sequelize('bnj1oun0ufiikmweczhj', 'urtphz16jxw2cvaofmxa', 'IlKZ958UPu3ePzMCTaeNTuhZgddw2K', {
-    host: 'bnj1oun0ufiikmweczhj-postgresql.services.clever-cloud.com',
+dotenv.config()
+const db = new Sequelize({
     dialect: 'postgres',
-    logging: false
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: false,
 });
 
 export default db;
