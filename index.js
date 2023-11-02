9b4a0906765fdb70ceb1b114c1722f3f8c352c35
gabrielsidauruk <gpratama796@gmail.com>
2023-11-02 20:30:31 +0700

    fix: add New CORS

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const app = express();
 const port = 8000;
 
 // Konfigurasi CORS untuk mengizinkan akses dari domain tertentu
-const corsOptions = {
-    origin: 'https://dempstershaferjurusan.vercel.app',
-    methods: 'GET, POST, PUT,PATCH, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: 'https://dempstershaferjurusan.vercel.app',
+//     methods: 'GET, POST, PUT,PATCH, DELETE',
+//     allowedHeaders: 'Content-Type, Authorization',
+// };
+// app.use(cors(corsOptions));
+// Middleware untuk mengizinkan semua domain (atau Anda dapat menentukan domain tertentu)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Mengizinkan semua domain
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(express.json());
 app.use(UserRoute);
 app.use(PertanyaanRoute);
