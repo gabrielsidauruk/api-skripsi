06613e627204edbb5a81d1b67fcf64f4507f7188
gabrielsidauruk <gpratama796@gmail.com>
2023-11-02 20:25:08 +0700

    add cors config

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import PertanyaanRoute from "./routes/PertanyaanRoute.js"
 const app = express();
 const port = 8000;
 
-app.use(cors());
+// Konfigurasi CORS untuk mengizinkan akses dari domain tertentu
+const corsOptions = {
+    origin: 'https://dempstershaferjurusan.vercel.app',
+    methods: 'GET, POST, PUT,PATCH, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(UserRoute);
 app.use(PertanyaanRoute);
